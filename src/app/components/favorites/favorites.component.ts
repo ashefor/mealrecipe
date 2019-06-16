9f930d7fed264175e8aa378883df3a7a6cc47d4f
Michael Ashefor <thatsureboy@gmail.com>
2019-06-16 02:02:48 +0100

    add function for removing favorites

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Component, OnInit, Input } from '@angular/core';
 import { RecipesService } from 'src/app/services/recipes.service';
@@ -17,24 +18,25 @@ export class FavoritesComponent implements OnInit {
   ingArray: any[] = [];
   msmArray: any[] = []
   newObj: { [key: string]: string } = {};
-  mealID;
-  showmore = false;
   nothingFound = false;
-  constructor(private service: RecipesService, private sanitizer: DomSanitizer) { }
+  showLoader = true;
+  constructor(private service: RecipesService, private sanitizer: DomSanitizer, private router: Router) { }
 
   ngOnInit() {
     const retrrievedData = localStorage.getItem('mealIds')
     this.favMeals = JSON.parse(retrrievedData)
     const newarray = new Set(this.favMeals)
-    console.log(this.favMeals)
     const fav = Array.from(newarray)
     if(fav.length < 1){
       this.nothingFound = true;
+      this.showLoader =  false;
     }
     for (let index of fav) {
       this.service.lookup(index).subscribe((res: any) => {
-        this.meals.push(res.meals[0])
-        // console.log(this.meals)
+        if(res){
+          this.showLoader = false;
+          this.meals.push(res.meals[0])
+        console.log(this.meals)
         let allObj = res.meals[0];
         const youTubeLink = allObj.strYoutube
         const cleanLink = youTubeLink.replace('watch?v=', 'embed/')
@@ -43,21 +45,27 @@ export class FavoritesComponent implements OnInit {
           this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.allcleanLinks[index])
         }
         this.arr.push(this.safeURL)
+        }
       })
     }
 
   }
+  clearsto(){
+    localStorage.clear()
+  }
   removeFromFav(e){
-    console.log(this.favMeals.indexOf(this.mealID))
-    const index = this.favMeals.indexOf(this.mealID)
+    // console.log(this.favMeals.indexOf(this.mealID))
+    let mealID = e.path[1].childNodes[0].value
+    const index = this.favMeals.indexOf(mealID)
+    console.log(index)
     if(index > -1 ){
-      // this.favMeals.splice(index, 1)
-
-    alert(this.mealID)
-      console.log(status)
+      this.favMeals.splice(index, 1)
       console.log(this.favMeals)
+      // this.router.navigate(['/favorites'])
+      // window.top.location = window.top.location
+      window.location.reload()
     }
-    // localStorage.setItem('mealIds', JSON.stringify(this.favMeals))
+    localStorage.setItem('mealIds', JSON.stringify(this.favMeals))
   }
 }
 
