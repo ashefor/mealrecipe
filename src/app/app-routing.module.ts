0cab4f4867a4ec6974fc41b5f232490d8ae71d87
Michael Ashefor <thatsureboy@gmail.com>
2019-06-16 03:50:55 +0100

    add cateroy to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { RandomrecipeComponent } from './components/randomrecipe/randomrecipe.co
 import { SearchComponent } from './components/search/search.component';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { FavoritesComponent } from './components/favorites/favorites.component';
+import { CategoryComponent } from './components/category/category.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'random', component: RandomrecipeComponent},
   {path: 'recipe/:id', component: RecipeComponent},
+  {path: 'category/:category', component: CategoryComponent},
   {path: 'favorites', component: FavoritesComponent},
   {path: 'search', component: SearchComponent, pathMatch: 'prefix'},
 ];
@@ -19,4 +21,10 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routeComponents = [HomeComponent, RandomrecipeComponent, SearchComponent, RecipeComponent, FavoritesComponent]
+export const routeComponents = [HomeComponent, 
+  RandomrecipeComponent, 
+  SearchComponent, 
+  RecipeComponent, 
+  FavoritesComponent,
+  CategoryComponent
+]
